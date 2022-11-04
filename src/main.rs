0c66c8547dcaa36eeb2688b1b1109ba87eca7c87
alagha-go <abdihakimosman1480@gmail.com>
2022-11-04 19:08:05 +0000

    testing movie impl functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use static_init::dynamic;
+
+mod crawler;
+mod movies;
+
+#[dynamic]
+static CLIENT: reqwest::Client = reqwest::Client::new();
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let movie = movies::Movie::default();
+    movie.save();
+    let json = serde_json::to_string(&movie).unwrap();
+    let value: movies::Movie = serde_json::from_str(&json).unwrap();
+    println!("{:#?}", value);
+    
+    Ok(())
 }
