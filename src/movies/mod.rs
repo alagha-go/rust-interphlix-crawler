c881c8187f554ff8febacc0c6d154b6a89e5bc6f
alagha-go <abdihakimosman1480@gmail.com>
2022-11-04 19:05:43 +0000

    imports and include necessary files

---
+++ src/movies/mod.rs
@@ -0,0 +1,12 @@
+use serde::{Deserialize, Serialize, Serializer, Deserializer};
+use serde::de::{self, Visitor};
+use std::fs::{self, *};
+use std::io::{BufWriter, Write};
+use std::fmt;
+use crate::crawler;
+
+include! {"structs.rs"}
+include! {"movie.rs"}
+include! {"collect.rs"}
+include! {"object_id.rs"}
+include! {"visitor.rs"}
