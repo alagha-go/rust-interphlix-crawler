aa853b97402e9fde3fa2a5146672b76b6048409b
alagha-go <abdihakimosman1480@gmail.com>
2022-11-04 18:58:13 +0000

    include all the necessary files

--- src/crawler/mod.rs
+++ src/crawler/mod.rs
@@ -1,6 +1,8 @@
 use html_parser::{Dom, Element, Node};
 
-include!("structs.rs");
-include!("document.rs");
-include!("collect.rs");
-include!("request.rs");
+include!{"structs.rs"}
+include!{"document.rs"}
+include!{"collect.rs"}
+include!{"request.rs"}
+include!{"content.rs"}
+include!{"elements.rs"}
