90486e8224657eec3a765f4129a8c891c3b3b63c
alagha-go <abdihakimosman1480@gmail.com>
2022-11-04 18:54:22 +0000

    return values

--- src/crawler/collect.rs
+++ src/crawler/collect.rs
@@ -1,22 +1,19 @@
 #[allow(dead_code)]
-pub async fn get_movies(html: String) -> Vec<(String, String, String)> {
-    let document = Document::new(&html);
+pub fn get_movies(html: &String) -> Vec<(String, String, String, String)> {
+    let document = Document::new(html);
     let documents = document.get_by_class(&"film_list-wrap".to_owned()).expect("could not find document").get_all_by_class(&"flw-item".to_owned());
 
-    let mut movies: Vec<(String, String, String)> = Vec::new();
+    let mut movies: Vec<(String, String, String, String)> = Vec::new();
 
     for document in documents {
-        match document {
-            Document::Element(element) => {
-                let film_name = Document::Element(element.clone()).get_by_class(&"film-detail".to_owned()).unwrap().get_by_class(&"film-name".to_owned()).unwrap();
-                let poster = Document::Element(element).get_by_class(&"film-poster".to_owned()).unwrap();
-                let page_url = "https://sflix.to".to_owned() + &film_name.child_attribute(&"href".to_owned()).unwrap();
-                let name = film_name.child_attribute(&"title".to_owned()).unwrap();
-                let image_url = poster.child_attribute(&"data-src".to_owned()).unwrap();
-                movies.push((name, page_url, image_url));
-            },
-            _ => {}
-        }
+        let film_name = document.get_by_class(&"film-detail".to_owned()).unwrap().get_by_class(&"film-name".to_owned()).unwrap();
+        let poster = document.get_by_class(&"film-poster".to_owned()).unwrap();
+        let page_url = "https://sflix.to".to_owned() + &film_name.child_attribute(&"href".to_owned()).unwrap();
+        let split: Vec<&str> = page_url.split("-").collect();
+        let code = split[split.len()-1].to_owned();
+        let name = film_name.child_attribute(&"title".to_owned()).unwrap();
+        let image_url = poster.child_attribute(&"data-src".to_owned()).unwrap();
+        movies.push((name, page_url, code, image_url));
     }
 
     movies
