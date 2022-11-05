339f385e75c50464f26084862a7b39752ce629ce
alagha-go <abdihakimosman1480@gmail.com>
2022-11-05 09:36:10 +0000

    collect seasons, episodes and servers

--- src/movies/collect.rs
+++ src/movies/collect.rs
@@ -1,6 +1,6 @@
-pub async fn collect_movies(url: &String) {
+pub async fn collect_movies(url: &String, movie_type: MovieType) {
     let mut tuples = Vec::new();
-    let length = crawler::get_pages_length(url.clone()).await;
+    let length = 2;//crawler::get_pages_length(url.clone()).await;
     
     let mut pool = Vec::new();
     
@@ -19,10 +19,63 @@ pub async fn collect_movies(url: &String) {
     let mut pool = Vec::new();
 
     for tuple in tuples {
-        pool.push(tokio::spawn(Movie::crawl(tuple, MovieType::Movie)));
+        pool.push(tokio::spawn(Movie::crawl(tuple, movie_type.clone())));
     }
 
     for handle in pool {
         movies.push(handle.await.unwrap());
     }
+
+    Movie::save_codes();
+}
+
+pub async fn collect_seasons(url: &String) -> Vec<Season> {
+    let html = crawler::get_request(url.to_owned()).await.unwrap();
+    let tuples = crawler::get_seasons(&html);
+    let mut seasons: Vec<Season> = Vec::new();
+
+    let mut pool = Vec::new();
+
+    for (name, code) in tuples {
+        let url = format!("{}{code}", &*crate::EPISODESURL);
+        pool.push(tokio::spawn(async move{
+            let episodes = collect_episodes(&url).await;
+            let id = ObjectId::new();
+            Season{id, name, code, episodes}
+        }));
+    }
+
+    for handle in pool {
+        seasons.push(handle.await.unwrap());
+    }
+    
+    seasons
+}
+
+pub async fn collect_episodes(url: &String) -> Vec<Episode> {
+    let html = crawler::get_request(url.to_owned()).await.unwrap();
+    let tuples = crawler::get_episodes(&html);
+    let mut episodes: Vec<Episode> = Vec::new();
+
+    for (number, name, code, image_url) in tuples {
+        let url = format!("{}{code}", &*crate::EPISODESERVERSURL);
+        let servers = collect_servers(&url).await;
+        let id = ObjectId::new();
+        episodes.push(Episode{id, number, name, code, image_url, servers});
+    }
+
+    episodes
+}
+
+pub async fn collect_servers(url: &String) -> Vec<Server> {
+    let html = crawler::get_request(url.to_owned()).await.unwrap();
+    let tuples = crawler::get_servers(&html);
+    let mut servers: Vec<Server> = Vec::new();
+
+    for (name, video_id) in tuples {
+        let id = ObjectId::new();
+        servers.push(Server{id, name, video_id});
+    }
+
+    servers
 }
