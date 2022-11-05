38ee39f9cfaa2e4a9927d26a4492642c9f902526
alagha-go <abdihakimosman1480@gmail.com>
2022-11-05 03:51:02 +0000

    get episode code from the html

--- src/crawler/elements.rs
+++ src/crawler/elements.rs
@@ -24,17 +24,18 @@ pub fn get_seasons(html: &String) -> Vec<(String, String)> {
     seasons.into_iter().rev().collect::<Vec<(String, String)>>()
 }
 
-pub fn get_episodes(html: &String) -> Vec<(String, String, String)> {
+pub fn get_episodes(html: &String) -> Vec<(i32, String, String, String)> {
     let documents = Document::new(html).get_all_by_class(&"swiper-slide".to_owned());
 
-    let mut episodes: Vec<(String, String, String)> = Vec::new();
+    let mut episodes: Vec<(i32, String, String, String)> = Vec::new();
 
     for document in documents {
         let image_url = document.get_by_name(&"img".to_owned()).unwrap().attribute(&"src".to_owned()).unwrap();
-        let episode_number = document.get_by_class(&"episode-number".to_owned()).unwrap().get_text().unwrap();
+        let code = document.child_attribute(&"data-id".to_owned()).unwrap();
+        let episode_number = document.get_by_class(&"episode-number".to_owned()).unwrap().get_text().unwrap().replace(":", "").replace("Episode ", "").replace("\n", "").parse::<i32>().unwrap();
         let name = document.get_by_class(&"film-name".to_owned()).unwrap().child_attribute(&"title".to_owned()).unwrap();
-        episodes.push((episode_number, name, image_url));
+        episodes.push((episode_number, name, code, image_url));
     }
 
-    episodes.into_iter().rev().collect::<Vec<(String, String, String)>>()
+    episodes.into_iter().rev().collect::<Vec<(i32, String, String, String)>>()
 }
